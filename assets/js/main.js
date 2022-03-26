a4069a76b96dd81241f6d5145a06dad2ef66eb16
Alexxboc <boccardi.alessandro@gmail.com>
2022-03-26 10:57:11 +0100

    Create price vars and start printing ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,31 +16,47 @@ const button = document.getElementById(`save_data`);
 button.addEventListener (`click`, 
 function(){
     console.log(`click`)
+// Chiedi all'utente il nome e il cognome
     let user_name = document.getElementById(`name`).value;
-    let km_da_percorrere = Number(document.getElementById("percorso").value);
+
+// Chiedi all'utente quanti km deve percorrere
+    let km_da_percorrere = Number(document.getElementById(`percorso`).value);
+    if (isNaN(km_da_percorrere)) {
+        alert(`Nel campo Km da percorrere scrivi un valore numerico`)
+    }
+// Chiedi all'utente la fascia d'età
     let user_age = document.getElementById(`fascia_eta`).value;
-    
-    console.log(user_name , km_da_percorrere , user_age);
-})
 
-// Calcolare il prezzo del biglietto standard
+// Calcola il prezzo standard
+    const prezzo_standard = km_da_percorrere * 0.21;
+    console.log(user_name , km_da_percorrere , user_age , prezzo_standard);
 
-const prezzo_standard = km_da_percorrere * 0.21;
-
-// Calcoloare prezzo finale  
-
-let prezzo_finale;
-
-if (user_age == `under 18`) {
+// Calcola il prezzo finale con l'eventuale sconto applicato
+    let prezzo_finale;
+    if (user_age == `under 18`) {
      prezzo_finale = prezzo_standard - (prezzo_standard * 0.2);
-    
-} else if (user_age == `over 65`) {
+    } else if (user_age == `over 65`) {
      prezzo_finale = prezzo_standard - (prezzo_standard * 0.4);
-} else {
+    } else {
     prezzo_finale = prezzo_standard;
 }
 
-console.log(`Il prezzo del tuo biglietto è di ${prezzo_finale.toFixed(2)} €`)
+// Comunica il prezzo finale all'utente
+console.log(`Ciao ${user_name} Il prezzo del tuo biglietto è di ${prezzo_finale.toFixed(2)} €`)
+
+// Stampa l'offerta sul biglietto in base alla fascia d'età
+const elementOfferta = document.querySelector(`.offerta`)
+if (user_age == `under 18`){
+    elementOfferta.innerHTML = `Sconto minorenne`
+} else if (user_age == `over 65`){
+    elementOfferta.innerHTML = `Sconto over`
+} else 
+    elementOfferta.innerHTML = `Standard`
+
+})
+
+
+
 
 
 
